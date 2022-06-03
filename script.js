a86dc8d406fba8c9d7b674fa36c2b32150803155
Frederico Gonçalves <64445839+fredsilva7l@users.noreply.github.com>
2022-06-03 20:07:41 -0300

    arquivos alterados

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function typeWrite(elemento){
-    const textoArray = elemento.innerHTML.split('');
-    elemento.innerHTML = ' ';
-    textoArray.forEach(function(letra, i){   
-      
-    setTimeout(function(){
-        elemento.innerHTML += letra;
-    }, 75 * i)
+  const textoArray = elemento.innerHTML.split('');
+  elemento.innerHTML = ' ';
+  textoArray.forEach(function(letra, i){   
+    
+  setTimeout(function(){
+      elemento.innerHTML += letra;
+  }, 50 * i)
 
-  });
+});
 }
 const titulo = document.querySelector('.maquinaEscrever');
 typeWrite(titulo);
