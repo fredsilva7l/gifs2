d594ca12b89b1e41dfda7d3fe73f2e79a4edb308
Frederico Gonçalves <64445839+fredsilva7l@users.noreply.github.com>
2022-06-04 00:31:51 -0300

    Adicionamo movimentos

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+
+var texto = document.querySelector(".maquinaEscrever");
+var rascunho = texto.textContent;
+console.log(rascunho.length);
+
 function typeWrite(elemento){
     const textoArray = elemento.innerHTML.split('');
     elemento.innerHTML = ' ';
@@ -5,9 +10,60 @@ function typeWrite(elemento){
       
     setTimeout(function(){
         elemento.innerHTML += letra;
-    }, 1 * i)
+    }, 100 * i)
 
   });
 }
+
+const corpo = document.querySelector('body');
+
 const titulo = document.querySelector('.maquinaEscrever');
 typeWrite(titulo);
+
+const coração = document.querySelector('.heart')
+const eco = document.querySelector('.echo')
+const nomeVariavel = document.querySelector('.nameMarcador')
+
+setTimeout(function() {
+  corpo.classList.add("corpo");
+}, 31000);
+
+setTimeout(function() {
+  titulo.classList.add("campoEscrita");
+}, 68000);
+
+setTimeout(function() {
+  titulo.classList.add("responsivoQuadro");
+  coração.classList.add("responsivoCoracao");
+  eco.classList.add("responsivoCoracao");
+}, 148000);
+
+setTimeout(function() {
+  coração.classList.add("halfHeart");
+  eco.classList.add("halfEcho");
+}, 202000);
+
+setTimeout(function() {
+  coração.classList.add("fullHeart");
+  eco.classList.add("fullEcho");
+}, 225600);
+
+setTimeout(function() {
+  coração.classList.add("shadowHeart");
+}, 244700);
+
+setTimeout(function() {
+  coração.classList.add("fullShadowHeart");
+  eco.classList.add("Echo");
+}, 253700);
+
+setTimeout(function() {
+  nomeVariavel.classList.add("nome");
+}, 289000);
+
+setTimeout(function() {
+  coração.classList.add("beatHeart");
+  eco.classList.add("beatEcho");
+}, 429000);
+
+
